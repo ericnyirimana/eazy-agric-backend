16f138f8958165e211c0fe7847e32d9ada20bd24
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-08-22 16:27:00 +0200

    EW-122-story(usage-report): count  the number of active mobile users
     - add the helper method to count active mobile users
     - add the controller to count active mobile users
     - add the route to display active mobile users
     - write integration test for the feature
     - document the feature on the README.md
    
    [Delivers EW-122]

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
@@ -10,56 +11,58 @@ use App\Http\Controllers\UserController;
 | and give it the Closure to call when that URI is requested.
 |
  */
+
 $router->get('/', function () use ($router) {
-  return $router->app->version();
+    return $router->app->version();
 });
 $router->group(['prefix' => 'api/v1'], function () use ($router) {
-  $router->post('/contact', 'ContactController@sendContactForm');
-  $router->group(['prefix' => 'auth'], function () use ($router) {
-    $router->post('/login', 'AuthController@authenticate');
-    $router->post('/forgot-password', 'AuthController@forgotPassword');
-    $router->post('/password-verification-token', 'AuthController@verifyResetPasswordToken');
-    $router->post('/resend-password', 'AuthController@forgotPassword');
-    $router->put('/confirm-password', 'AuthController@confirmPassword');
-  });
-  $router->group(['middleware' => 'auth'], function () use ($router) {
-    $router->group(['middleware' => 'admin'], function () use ($router) {
-      $router->group(['middleware' => 'validateParams'], function () use ($router) {
-        $router->post('/users/{user}/', 'UserController@createUser');
-      });
-      $router->get('/active-users', 'UserController@getAllActiveUsers');
-      $router->post('/change-password', 'AdminController@changePassword');
-      $router->post('/admin', 'AdminController@createAdmin');
-      $router->get('/masteragents', 'MasterAgentController@getMasterAgents');
-      $router->post('/devt-partners', 'DevtPartnerController@createDevtPartner');
-      $router->get('/users/{user}', 'AdminController@getUsers');
-      $router->get('/devt-partners', 'DevtPartnerController@getDevtPartners');
-      $router->get('/top-districts', 'DistrictController@getTopDistricts');
-      $router->get('/activity-summary', 'ActivityController@getActivitySummary');
-      $router->get('/total-acreage', 'MapCordinatesController@getTotalAcreage');
-      $router->get('/total-payment', 'TotalPaymentController@getTotalPayment');
-      $router->get('/twitter-report', 'UserController@getTwitterReport');
-      $router->get('/youtube-report', 'UserController@getYoutubeReport');
-      $router->get('/facebook-report', 'UserController@getFacebookReport');
-      $router->get('/admins', 'AdminController@getAdmins');
-      $router->delete('/account/{id}', 'AdminController@deleteAccount');
-      $router->patch('/account/{id}', 'AdminController@editAccount');
-      $router->get('/top-produce', 'FarmerProduceController@getTopFarmProduce');
-      $router->get('/top-performing/{agent}', 'AdminController@getTopAgents');
-      $router->get('/top-performing-district', 'DistrictController@getTopPerformingDistricts');
-      $router->get('/account/{id}', 'AdminController@getUser');
-      $router->get('/districts', 'DistrictController@getDistricts');
-      $router->get('/enterprises', 'OurCropController@getEnterprises');
-      $router->get('/most-ordered', 'ReportController@getMostOrdered');
-      $router->get('/farmer-agents-order-statistics', 'ReportController@getFarmerAgentsOrderStatistics');
-      $router->patch('/{action}/{id}', 'AdminController@accountAction');
-      $router->post('/village-agents', 'UserController@addVillageAgents');
-      $router->get('/farmers', 'FarmerController@getFarmers');
-      $router->get('/visitor', 'AnalyticsController@getNumberOfVistors');
+    $router->post('/contact', 'ContactController@sendContactForm');
+    $router->group(['prefix' => 'auth'], function () use ($router) {
+        $router->post('/login', 'AuthController@authenticate');
+        $router->post('/forgot-password', 'AuthController@forgotPassword');
+        $router->post('/password-verification-token', 'AuthController@verifyResetPasswordToken');
+        $router->post('/resend-password', 'AuthController@forgotPassword');
+        $router->put('/confirm-password', 'AuthController@confirmPassword');
     });
-    $router->get('/activity-log', 'ActivityController@getActivityLog');
-  });
-  $router->group(['middleware' => 'validateParams'], function () use ($router) {
-    $router->post('/request/{user}', 'UserController@requestAccount');
-  });
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->group(['middleware' => 'admin'], function () use ($router) {
+            $router->group(['middleware' => 'validateParams'], function () use ($router) {
+                $router->post('/users/{user}/', 'UserController@createUser');
+            });
+            $router->get('/active-users', 'UserController@getAllActiveUsers');
+            $router->get('/active-mobile-users', 'UserController@getActiveMobileUsers');
+            $router->post('/change-password', 'AdminController@changePassword');
+            $router->post('/admin', 'AdminController@createAdmin');
+            $router->get('/masteragents', 'MasterAgentController@getMasterAgents');
+            $router->post('/devt-partners', 'DevtPartnerController@createDevtPartner');
+            $router->get('/users/{user}', 'AdminController@getUsers');
+            $router->get('/devt-partners', 'DevtPartnerController@getDevtPartners');
+            $router->get('/top-districts', 'DistrictController@getTopDistricts');
+            $router->get('/activity-summary', 'ActivityController@getActivitySummary');
+            $router->get('/total-acreage', 'MapCordinatesController@getTotalAcreage');
+            $router->get('/total-payment', 'TotalPaymentController@getTotalPayment');
+            $router->get('/twitter-report', 'UserController@getTwitterReport');
+            $router->get('/youtube-report', 'UserController@getYoutubeReport');
+            $router->get('/facebook-report', 'UserController@getFacebookReport');
+            $router->get('/admins', 'AdminController@getAdmins');
+            $router->delete('/account/{id}', 'AdminController@deleteAccount');
+            $router->patch('/account/{id}', 'AdminController@editAccount');
+            $router->get('/top-produce', 'FarmerProduceController@getTopFarmProduce');
+            $router->get('/top-performing/{agent}', 'AdminController@getTopAgents');
+            $router->get('/top-performing-district', 'DistrictController@getTopPerformingDistricts');
+            $router->get('/account/{id}', 'AdminController@getUser');
+            $router->get('/districts', 'DistrictController@getDistricts');
+            $router->get('/enterprises', 'OurCropController@getEnterprises');
+            $router->get('/most-ordered', 'ReportController@getMostOrdered');
+            $router->get('/farmer-agents-order-statistics', 'ReportController@getFarmerAgentsOrderStatistics');
+            $router->patch('/{action}/{id}', 'AdminController@accountAction');
+            $router->post('/village-agents', 'UserController@addVillageAgents');
+            $router->get('/farmers', 'FarmerController@getFarmers');
+            $router->get('/visitor', 'AnalyticsController@getNumberOfVistors');
+        });
+        $router->get('/activity-log', 'ActivityController@getActivityLog');
+    });
+    $router->group(['middleware' => 'validateParams'], function () use ($router) {
+        $router->post('/request/{user}', 'UserController@requestAccount');
+    });
 });
