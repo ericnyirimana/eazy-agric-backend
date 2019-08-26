edb6dc2f7dffa9b1113d446a5af57d3e13204f13
AtamaZack <atama.zack@hotmail.com>
2019-08-26 09:50:58 +0300

    EW-185-fix(farmers list): modify districts stats endpoint
    this commit modifies the districts statistics endpoint to allow filtering by district
    
    [Fixes EW-185]

--- routes/web.php
+++ routes/web.php
@@ -16,53 +16,54 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 $router->group(['prefix' => 'api/v1'], function () use ($router) {
-    $router->post('/contact', 'ContactController@sendContactForm');
-    $router->group(['prefix' => 'auth'], function () use ($router) {
-        $router->post('/login', 'AuthController@authenticate');
-        $router->post('/forgot-password', 'AuthController@forgotPassword');
-        $router->post('/password-verification-token', 'AuthController@verifyResetPasswordToken');
-        $router->post('/resend-password', 'AuthController@forgotPassword');
-        $router->put('/confirm-password', 'AuthController@confirmPassword');
+  $router->post('/contact', 'ContactController@sendContactForm');
+  $router->group(['prefix' => 'auth'], function () use ($router) {
+    $router->post('/login', 'AuthController@authenticate');
+    $router->post('/forgot-password', 'AuthController@forgotPassword');
+    $router->post('/password-verification-token', 'AuthController@verifyResetPasswordToken');
+    $router->post('/resend-password', 'AuthController@forgotPassword');
+    $router->put('/confirm-password', 'AuthController@confirmPassword');
+  });
+  $router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->group(['middleware' => 'admin'], function () use ($router) {
+      $router->group(['middleware' => 'validateParams'], function () use ($router) {
+        $router->post('/users/{user}/', 'UserController@createUser');
+      });
+      $router->get('/active-users', 'UserController@getAllActiveUsers');
+      $router->get('/active-mobile-users', 'UserController@getActiveMobileUsers');
+      $router->post('/change-password', 'AdminController@changePassword');
+      $router->post('/admin', 'AdminController@createAdmin');
+      $router->get('/masteragents', 'MasterAgentController@getMasterAgents');
+      $router->post('/devt-partners', 'DevtPartnerController@createDevtPartner');
+      $router->get('/users/{user}', 'AdminController@getUsers');
+      $router->get('/devt-partners', 'DevtPartnerController@getDevtPartners');
+      $router->get('/top-districts', 'DistrictController@getTopDistricts');
+      $router->get('/activity-summary', 'ActivityController@getActivitySummary');
+      $router->get('/total-acreage', 'MapCordinatesController@getTotalAcreage');
+      $router->get('/total-payment', 'TotalPaymentController@getTotalPayment');
+      $router->get('/twitter-report', 'UserController@getTwitterReport');
+      $router->get('/youtube-report', 'UserController@getYoutubeReport');
+      $router->get('/facebook-report', 'UserController@getFacebookReport');
+      $router->get('/admins', 'AdminController@getAdmins');
+      $router->delete('/account/{id}', 'AdminController@deleteAccount');
+      $router->patch('/account/{id}', 'AdminController@editAccount');
+      $router->get('/top-produce', 'FarmerProduceController@getTopFarmProduce');
+      $router->get('/top-performing/{agent}', 'AdminController@getTopAgents');
+      $router->get('/top-performing-district', 'DistrictController@getTopPerformingDistricts');
+      $router->get('/top-performing-district/{district}', 'DistrictController@getTopPerformingDistricts');
+      $router->get('/account/{id}', 'AdminController@getUser');
+      $router->get('/districts', 'DistrictController@getDistricts');
+      $router->get('/enterprises', 'OurCropController@getEnterprises');
+      $router->get('/most-ordered', 'ReportController@getMostOrdered');
+      $router->get('/farmer-agents-order-statistics', 'ReportController@getFarmerAgentsOrderStatistics');
+      $router->patch('/{action}/{id}', 'AdminController@accountAction');
+      $router->post('/village-agents', 'UserController@addVillageAgents');
+      $router->get('/farmers', 'FarmerController@getFarmers');
+      $router->get('/visitor', 'AnalyticsController@getNumberOfVistors');
+      $router->get('/orders/completed', 'OrderController@getCompletedOrders');
+      $router->get('/farmers-orders', 'FarmersOrderController@getNumberOfFarmersWhoOrderedDifferentInputCategories');
     });
-    $router->group(['middleware' => 'auth'], function () use ($router) {
-        $router->group(['middleware' => 'admin'], function () use ($router) {
-            $router->group(['middleware' => 'validateParams'], function () use ($router) {
-                $router->post('/users/{user}/', 'UserController@createUser');
-            });
-            $router->get('/active-users', 'UserController@getAllActiveUsers');
-            $router->get('/active-mobile-users', 'UserController@getActiveMobileUsers');
-            $router->post('/change-password', 'AdminController@changePassword');
-            $router->post('/admin', 'AdminController@createAdmin');
-            $router->get('/masteragents', 'MasterAgentController@getMasterAgents');
-            $router->post('/devt-partners', 'DevtPartnerController@createDevtPartner');
-            $router->get('/users/{user}', 'AdminController@getUsers');
-            $router->get('/devt-partners', 'DevtPartnerController@getDevtPartners');
-            $router->get('/top-districts', 'DistrictController@getTopDistricts');
-            $router->get('/activity-summary', 'ActivityController@getActivitySummary');
-            $router->get('/total-acreage', 'MapCordinatesController@getTotalAcreage');
-            $router->get('/total-payment', 'TotalPaymentController@getTotalPayment');
-            $router->get('/twitter-report', 'UserController@getTwitterReport');
-            $router->get('/youtube-report', 'UserController@getYoutubeReport');
-            $router->get('/facebook-report', 'UserController@getFacebookReport');
-            $router->get('/admins', 'AdminController@getAdmins');
-            $router->delete('/account/{id}', 'AdminController@deleteAccount');
-            $router->patch('/account/{id}', 'AdminController@editAccount');
-            $router->get('/top-produce', 'FarmerProduceController@getTopFarmProduce');
-            $router->get('/top-performing/{agent}', 'AdminController@getTopAgents');
-            $router->get('/top-performing-district', 'DistrictController@getTopPerformingDistricts');
-            $router->get('/account/{id}', 'AdminController@getUser');
-            $router->get('/districts', 'DistrictController@getDistricts');
-            $router->get('/enterprises', 'OurCropController@getEnterprises');
-            $router->get('/most-ordered', 'ReportController@getMostOrdered');
-            $router->get('/farmer-agents-order-statistics', 'ReportController@getFarmerAgentsOrderStatistics');
-            $router->patch('/{action}/{id}', 'AdminController@accountAction');
-            $router->post('/village-agents', 'UserController@addVillageAgents');
-            $router->get('/farmers', 'FarmerController@getFarmers');
-            $router->get('/visitor', 'AnalyticsController@getNumberOfVistors');
-            $router->get('/orders/completed', 'OrderController@getCompletedOrders');
-            $router->get('/farmers-orders', 'FarmersOrderController@getNumberOfFarmersWhoOrderedDifferentInputCategories');
-          });
-        $router->get('/activity-log', 'ActivityController@getActivityLog');
+    $router->get('/activity-log', 'ActivityController@getActivityLog');
     });
     $router->group(['middleware' => 'validateParams'], function () use ($router) {
         $router->post('/request/{user}', 'UserController@requestAccount');
